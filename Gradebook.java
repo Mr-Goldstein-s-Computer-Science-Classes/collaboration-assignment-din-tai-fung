e95155f2edd7fc412312ed975e76487bc166ecb1
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-21 12:29:28 -0500

    Updated Gradebook

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,25 @@
-public class Gradebook {
+import java.util.ArrayList;
+
+public class Gradebook
+{
+    private ArrayList<String> assignments = new ArrayList<String>();
+    public Gradebook()
+    {
+    }
+    public void createStudent(String name)
+    {
+        //Student student = new Student(name);
+    }
+    public void addAssignment(String assignment)
+    {
+        assignments.add(assignment);
+    }
+    public void gradeAssignment(String assignment, String studentName, double grade)
+    {
+
+    }
+    public double getAverageGrade(String assignment)
+    {
+        
+    }
 }
