273ac21709427da7df9bce118e4aad13885d795e
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-21 12:54:25 -0500

    updategradbook

--- Gradebook.java
+++ Gradebook.java
@@ -2,24 +2,26 @@ import java.util.ArrayList;
 
 public class Gradebook
 {
-    private ArrayList<String> assignments = new ArrayList<String>();
+    private ArrayList<Student> narnia = new ArrayList<Student>();
     public Gradebook()
     {
     }
     public void createStudent(String name)
     {
-        //Student student = new Student(name);
+        Student student = new Student(name);
+        ArrayList<>
     }
     public void addAssignment(String assignment)
     {
+
         assignments.add(assignment);
     }
     public void gradeAssignment(String assignment, String studentName, double grade)
     {
-
+        
     }
     public double getAverageGrade(String assignment)
     {
-        
+
     }
 }
